f3e44bf521558880e284a8f2ea11d60881901adc
Francisco Di Nucci <franciscoldinucci@gmail.com>
2023-07-26 19:32:59 -0300

    onrender update en script.js

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server -> bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://copiadegpt.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -95,7 +95,7 @@ const handleSubmit = async (e) => {
   if(response.ok) {
     const data = await response.json();
     const parsedData = data.bot.trim();
-
+ 
     typeText(messageDiv, parsedData);
   } else {
     const err = await response.text();
